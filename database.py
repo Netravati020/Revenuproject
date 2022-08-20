231b93afc34ee262f963664cbf26cff44d2c5344
Netravati020 <85023998+Netravati020@users.noreply.github.com>
2022-08-20 11:14:47 +0530

    updated
    updated

---
+++ database.py
@@ -0,0 +1,13 @@
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from sqlalchemy.ext.declarative import declarative_base
+
+sql_database_url= "mysql+pymysql://root@localhost:3306/revenue"
+engine= create_engine(sql_database_url)
+SessionLocal= sessionmaker(autocommit=False,autoflush=False, bind=engine)
+
+
+Base= declarative_base()
+print("connected")
